0e042f2a98ef2f9b9111b37356b66830d859bf4b
Eric <ericch369@gmail.com>
2026-08-19 17:19:46 +0800

    Rename Robot and added greeting

---
+++ src/main/java/Rice.java
@@ -0,0 +1,9 @@
+public class Rice {
+    public static void main(String[] args) {
+        String Message = "Hello! I'm Rice.\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!\n";
+
+        System.out.println(Message);
+    }
+}
