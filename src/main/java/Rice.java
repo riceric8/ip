193d7059ad818d273b77062f319647f1d5553578
Eric <ericch369@gmail.com>
2026-08-19 23:58:19 +0800

    Added list and adding of tasks feature

--- src/main/java/Rice.java
+++ src/main/java/Rice.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 
 public class Rice {
+
+    static int currEmptyPos = 0;
+
     public static void main(String[] args) {
+        String[] list = new String[100];
+
         String message = "Hello! I'm Rice.\n"
                 + "What can I do for you?\n";
         System.out.println(message);
@@ -11,11 +16,21 @@ public class Rice {
 
         while (scanner.hasNextLine()) {
           String userInput = scanner.nextLine();
+
           if (userInput.equals("bye")) {
             System.out.println("  Bye. Hope to see you again soon!");
             break;
           }
-            System.out.println("  " + userInput);
+
+          if (userInput.equals("list")) {
+            for (int i = 0; i < currEmptyPos; i += 1) {
+                System.out.println(String.format("  %d. %s", i + 1, list[i]));
+            }
+          } else {
+            list[currEmptyPos] = userInput;
+            currEmptyPos += 1;
+            System.out.println("  added: " + userInput);
+            }
         }
     }
 }
