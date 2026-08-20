90377e9c0c6aa15e4c4a5a28019d6bc2f7ab1187
Eric <ericch369@gmail.com>
2026-08-20 10:55:37 +0800

    added mark-as-done feature

--- src/main/java/Rice.java
+++ src/main/java/Rice.java
@@ -6,10 +6,11 @@ public class Rice {
     static int currEmptyPos = 0;
 
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
 
         String message = "Hello! I'm Rice.\n"
                 + "What can I do for you?\n";
+
         System.out.println(message);
         
         Scanner scanner = new Scanner(System.in);
@@ -24,12 +25,22 @@ public class Rice {
 
           if (userInput.equals("list")) {
             for (int i = 0; i < currEmptyPos; i += 1) {
-                System.out.println(String.format("  %d. %s", i + 1, list[i]));
+              System.out.println(String.format("%d.%s", i + 1, list[i]));
             }
+          } else if (userInput.split(" ")[0].equals("mark")) { //split string, userInput should be immutable so this is another copy
+              int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+              list[taskNumber - 1].mark();
+              System.out.println("  Nice! I've marked this task as done:");
+              System.out.println("    " + list[taskNumber - 1].toString());
+          } else if (userInput.split(" ")[0].equals("unmark")) { 
+              int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+              list[taskNumber - 1].unmark();
+              System.out.println("  OK, I've marked this task as not done yet:");
+              System.out.println("    " + list[taskNumber - 1].toString());
           } else {
-            list[currEmptyPos] = userInput;
-            currEmptyPos += 1;
-            System.out.println("  added: " + userInput);
+              list[currEmptyPos] = new Task(userInput);
+              currEmptyPos += 1;
+              System.out.println("  added: " + userInput);
             }
         }
     }
