939badfee3d0f82a46a498d82dbd0119a9aabfe2
Eric <ericch369@gmail.com>
2026-08-28 00:50:14 +0800

    Implement Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ui {
+  private final Scanner scanner = new Scanner(System.in);
+  
+  public void greet() {
+    String message = "Hello! I'm Rice.\n"
+                + "What can I do for you?\n";
+    System.out.println(message);
+    System.out.println("Dates should be recorded in the format YYYY-MM-DD");
+  }
+
+
+  public String readInput() {
+      String userInput = scanner.nextLine();
+      return userInput;
+  }
+
+  public void show(String message) { System.out.println(message); }
+}
