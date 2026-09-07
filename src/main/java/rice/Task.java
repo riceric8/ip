80eb37789dd6b52d91f6dea5d8a2facdef18210e
Eric <ericch369@gmail.com>
2026-09-07 21:23:11 +0800

    Rename boolean

--- src/main/java/rice/Task.java
+++ src/main/java/rice/Task.java
@@ -1,11 +1,11 @@
 package rice;
 
 /**
- * Represents a task with a description and completion status.
+ * Represents a task with a description and completion isDone.
  */
 public class Task {
     private String task;
-    private boolean status = false;
+    private boolean isDone = false;
 
     /**
      * Creates an incomplete task with the given description.
@@ -20,29 +20,29 @@ public class Task {
      * Marks this task as complete.
      */
     public void mark() {
-        this.status = true;
+        this.isDone = true;
     }
 
     /**
      * Marks this task as incomplete.
      */
     public void unmark() {
-        this.status = false;
+        this.isDone = false;
     }
 
     public String getTask() {
         return this.task;
     }
 
-    public boolean getStatus() {
-        return this.status;
+    public boolean getisDone() {
+        return this.isDone;
     }
 
     @Override
     public String toString() {
         String completion = "";
 
-        if (this.status) {
+        if (this.isDone) {
             completion = "[X]";
         } else {
             completion = "[ ]";
